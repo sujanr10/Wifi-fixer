6e1b60fb151d5dd4eae4cd40b2706e3f3c53d04c
Sujanr10 <210179@pennmanor.net>
2021-03-22 18:47:48 -0400

    Create main python file fixed some things

--- Wifi-Fix/socket-client.py
+++ Wifi-Fix/socket-client.py
@@ -1,76 +1,47 @@
 import socket
 import time
 import os
-from datetime import datetime
-
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 80
 status = " "
 confirmation = " "
+ask = " "
+
+
 while True:
     try:
-        sock.connect(("google.com", port))
+        sock.connect(("www.google.com", port))
         status = "online"
         if status == "online":
             print("You are currently online")
         break
-    except:
+    except OSError:
         status = "offline"
         if status == "offline":
             print("You are currently offline")
-            UserI = input("Would you like to restart you're computer? (Yes/No): ")
-            if UserI == "Yes" or UserI == "yes":
-                confirmation = input("Are you sure? (It will close all you're application currently opened) Yes/No: ")
-                if confirmation == "Yes" or confirmatiom == "YES":
-                    print("downloading Internet tools")
-                    os.system("gnome-terminal -e 'sudo apt-get install -y net-tools'")
-                    print("Restarting your device")
-                    time.sleep(5)
+            ask = input("Would you like to run the program? (Yes/No):")
+            if ask == "YES" or ask == "Yes" or ask == "yes":
+                UserI = input("Would you like to restart you're computer? (Yes/No):")
+                if UserI == "Yes" or UserI == "yes":
                     os.system("gnome-terminal -e 'sudo shutdown -r now'")
-            elif UserI == "No" or UserI == "no":
-                UserI = input("Are you sure you want to close the program? Yes/No:")
-                if UserI == "Yes" or UserI == "yes" or UserI == "YES":
-                    print("closing program")
-                    time.sleep(5)
-                    break
-                if UserI == "No" or UserI == "NO":
-                    UserI = input("Would you like to restart you're computer? (Yes/No): ")
-                    if UserI == "Yes" or UserI == "YES":
-                        print("restarting your device")
+                elif UserI == "No" or UserI == "no":
+                    UserI = input("Are you sure you want to close the program? Yes/No:")
+                    if UserI == "Yes" or UserI == "yes" or UserI == "YES":
+                        print("closing program")
+                        time.sleep(5)
+                        os.system("gnome-terminal -e 'sudo shutdown -r now'")
+                        break
+                    if UserI == "No" or UserI == "NO":
+                        UserI = input("Would you like to restart you're computer? (Yes/No): ")
+                        if UserI == "Yes" or UserI == "YES":
+                            print("restarting your device")
             else:
                 print("Please type Yes or No")
                 UserI = input("Would you like to restart you're computer? (Yes/No): ")
                 if UserI == "Yes" or UserI == "yes":
-                    confirmation = input(
-                        "Are you sure? (It will close all you're application currently opened) Yes/No: ")
-                    if confirmation == "Yes" or confirmatiom == "YES":
-                        print("Restarting your device")
-                        time.sleep(5)
-                        os.system("gnome-terminal -e 'sudo shutdown -r now'")
-                    elif UserI == "No" or UserI == "no":
-                        print("closing program")
-                        time.sleep(5)
-                        break
-
-        sleep.time(10)
-        if status == "offline":
-            UserI = input("Would you like to disable/enable your internet? Yes/No")
-            if UserI == "YES" or UserI == "Yes" or UserI == "yes":
-                print("disabling/enabling internet")
-                time.sleep(5)
-                os.system("gnome-terminal -e 'sudo ifconfig wlp2s0 down'")
-                time.sleep(2)
-                os.system("gnome-terminal -e 'sudo ifconfig wlp2s0 up'")
-
-
-
-
-while True:
-    time.sleep(30)
-    timenow = now.strftime("%H:%M:%S")
-    try:
-        sock.connect(("google.com", port))
-        status = "online"
-        if status == "online":
-            print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+                     os.system("gnome-terminal -e 'sudo shutdown -r now'")
+                elif UserI == "No" or UserI == "no":
+                    print("closing program")
+                    time.sleep(5)
+                    break
